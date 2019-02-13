74420b2949a122f561bf4147a8d1090084a24c4c
fdolzanes1 <fdolzanes1@gmail.com>
2019-02-13 16:32:06 -0400

    modificando o controle de pausa e reparando o problema de interface

--- Assets/Scritpt/Gameplay/ControleDePausa.cs
+++ Assets/Scritpt/Gameplay/ControleDePausa.cs
@@ -6,31 +6,57 @@ public class ControleDePausa : MonoBehaviour
 {
     [SerializeField]
     private GameObject painelPause;
-    
+
+    private bool jogoEstaParado;
+
     private void Update()
     {
         if (this.EstaoTocandoNaTela())
+        {
+            if (this.jogoEstaParado)
+            {
+                this.ContinuarJogo();
+            }
+        }
+        else
+        {
+            if (!this.jogoEstaParado)
+            {
+                this.PararJogo();
+            }
+        }
+
+        /*if (this.EstaoTocandoNaTela())
         {
             this.ContinuarJogo();
         } 
         else
         {
             this.PararJogo();
-        }
-        
+        }*/
+
     }
 
     private void ContinuarJogo()
     {
-        this.painelPause.SetActive(false);
+        //this.painelPause.SetActive(false);
+        //this.MudarEscalaDeTempo(1);
+        StartCoroutine(this.EsperarEContinuarOJogo());
+    }
+
+    private IEnumerator EsperarEContinuarOJogo()
+    {
+        yield return new WaitForSecondsRealtime(0.2f);
         this.MudarEscalaDeTempo(1);
+        this.painelPause.SetActive(false);
+        this.jogoEstaParado = false;
     }
 
     private void PararJogo()
     {
         this.painelPause.SetActive(true);
         this.MudarEscalaDeTempo(0.1f);
-
+        this.jogoEstaParado = true;
     }
 
     private bool EstaoTocandoNaTela()

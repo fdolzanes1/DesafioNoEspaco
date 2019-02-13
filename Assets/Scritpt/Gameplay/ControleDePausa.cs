a2b03a976fed63192d500f020aa3134aa1495bcc
fdolzanes1 <fdolzanes1@gmail.com>
2019-02-13 16:16:55 -0400

    Controle de Pause e Slow Motion

---
+++ Assets/Scritpt/Gameplay/ControleDePausa.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleDePausa : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject painelPause;
+    
+    private void Update()
+    {
+        if (this.EstaoTocandoNaTela())
+        {
+            this.ContinuarJogo();
+        } 
+        else
+        {
+            this.PararJogo();
+        }
+        
+    }
+
+    private void ContinuarJogo()
+    {
+        this.painelPause.SetActive(false);
+        this.MudarEscalaDeTempo(1);
+    }
+
+    private void PararJogo()
+    {
+        this.painelPause.SetActive(true);
+        this.MudarEscalaDeTempo(0.1f);
+
+    }
+
+    private bool EstaoTocandoNaTela()
+    {
+        //return Input.touchCount > 0;
+        return Input.anyKey;
+    }
+
+    private void MudarEscalaDeTempo(float escala)
+    {
+        Time.timeScale = escala;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+    }
+
+}

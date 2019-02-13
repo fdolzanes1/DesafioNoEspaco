f574cce49ddff8e9b93abb7694b49610ed6c23eb
fdolzanes1 <fdolzanes1@gmail.com>
2019-02-13 10:48:56 -0400

    nave seguindo o mouse

---
+++ Assets/Scritpt/Gameplay/SegueMouse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegueMouse : MonoBehaviour
+{
+
+    void Update()
+    {
+        //posicao do mouse em relacao a ponto de tela convertendo em Vector2
+        var posicao = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        this.transform.position = posicao; 
+    }
+}

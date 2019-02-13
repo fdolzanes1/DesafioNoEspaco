6a553b8514c4b668acce26d847f8b7931bba1af9
fdolzanes1 <fdolzanes1@gmail.com>
2019-02-13 11:05:51 -0400

    seguindo alvo

---
+++ Assets/Scritpt/Gameplay/Seguir.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seguir : MonoBehaviour
+{
+    [SerializeField]
+    private Transform alvo;
+
+    void Update()
+    {
+        var deslocamento = alvo.position - this.transform.position;
+        deslocamento = deslocamento.normalized;
+        this.transform.position += deslocamento * Time.deltaTime;
+    }
+}
